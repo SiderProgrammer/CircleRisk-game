d9274c204da2d4a59a8f875bc512e4b434fa19c2
Patryk Murawski <patryk.murawski@booming-games.com>
2024-08-07 14:09:55 +0200

    fix client and server

--- server/src/database-manager.js
+++ server/src/database-manager.js
@@ -1,154 +1,145 @@
 const mongoose = require("mongoose")
 
-const srvConfig = require("./settings/db-config")
+//const srvConfig = require("./settings/db-config")
+
 const { Accounts, Levels } = require("./settings/db-models")
 
 const defaultAccountConfig = require("./settings/account-default-db")
 
-const DATABASE_URL = `mongodb+srv://${srvConfig.USERNAME}:${srvConfig.PASSWORD}@${srvConfig.HOST}/${srvConfig.DB}?retryWrites=true&w=majority`
+const DATABASE_URL = require("./settings/db-config") // process.env.DB_URL //  `mongodb+srv://${srvConfig.USERNAME}:${srvConfig.PASSWORD}@${srvConfig.HOST}/${srvConfig.DB}?retryWrites=true&w=majority`
 
 class DatabaseManager {
-  constructor() {
-  }
+  constructor() {}
   connectDatabase() {
-
-
     mongoose.connect(
       DATABASE_URL,
 
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useCreateIndex:true,
-        useFindAndModify:false,
-       //  poolSize: 10
+        useCreateIndex: true,
+        useFindAndModify: false,
+        //  poolSize: 10
         // autoIndex: false,
       },
       () => {}
     )
   }
-  
 
   createAccount(req, res) {
     // use double destruction in req
     const nickname = req.body.nickname
 
-    Accounts.exists({_id:nickname})
-    .then((is_exsisting) => {
+    Accounts.exists({ _id: nickname }).then((is_exsisting) => {
       if (is_exsisting) {
         res.sendStatus(403)
       } else {
+        const Promises = []
 
-       const Promises = [] 
-
-        Promises[0] = Accounts.create( 
-          {
-            _id:nickname,
-            ...defaultAccountConfig,
-          })
+        Promises[0] = Accounts.create({
+          _id: nickname,
+          ...defaultAccountConfig,
+        })
 
         Promises[1] = Levels.create({
           nickname,
-          score:0,
+          score: 0,
         })
 
         Promise.all(Promises).then(() => res.sendStatus(200))
-      //  console.log("new account created")
+        //  console.log("new account created")
       }
     })
   }
 
   getAccountProgress(req, res) {
     const nickname = req.body.nickname
-    Accounts.findOne({_id:nickname},
-      (err,account_data)=>{
- 
+    Accounts.findOne({ _id: nickname }, (err, account_data) => {
       //   console.log(nickname + "  Joined")
-        res.status(200).json(account_data)
-      }).lean().select("-_id")
-
+      res.status(200).json(account_data)
+    })
+      .lean()
+      .select("-_id")
   }
 
   saveMoney(req, res) {
-    const {money,nickname} = req.body;
-    
-   Accounts.updateOne({_id:nickname},{money},()=>res.sendStatus(200))
-    
-    }
-  
-    saveNewSkin(req, res) {
-      const nickname = req.body.nickname
-      const [skin_part,skin_number] = [req.body.skin[0],req.body.skin[1]]
-      const expression = {}
-      expression[`skins.${skin_part}`] = skin_number
+    const { money, nickname } = req.body
 
-      Accounts.updateOne({_id:nickname},{$push:expression}, () => res.sendStatus(200))
-  
-    }
-  
-    equipSkin(req, res) {
-      const {nickname,current_skins} = req.body
-      Accounts.updateOne(
-        { _id:nickname},
-        { current_skins},
-        ()=>res.sendStatus(200)
-      )
-       
-    }
+    Accounts.updateOne({ _id: nickname }, { money }, () => res.sendStatus(200))
+  }
 
-    getAccountScores(req,res){
-      const {level,nickname} = req.body
-      const query = {nickname}
+  saveNewSkin(req, res) {
+    const nickname = req.body.nickname
+    const [skin_part, skin_number] = [req.body.skin[0], req.body.skin[1]]
+    const expression = {}
+    expression[`skins.${skin_part}`] = skin_number
 
-      if(level) query.level = level
-//if level omitted, each level is found and returned
-    
-      Levels.find(query,(err,c)=>res.json(c)).lean().select("level score -_id")
-    }
-    postLevelScore(req, res) {
+    Accounts.updateOne({ _id: nickname }, { $push: expression }, () =>
+      res.sendStatus(200)
+    )
+  }
 
-      const { score, nickname, level } = req.body
-      const query = {nickname, level}
-      const options = { upsert: true}
-      const update = {score:score}
-  
-//console.log(score,nickname,level)
-      Levels.updateOne(query, update, options,()=>res.sendStatus(200))
- 
-    }
+  equipSkin(req, res) {
+    const { nickname, current_skins } = req.body
+    Accounts.updateOne({ _id: nickname }, { current_skins }, () =>
+      res.sendStatus(200)
+    )
+  }
 
-    getTopScores(req,res){
-      const {level,players_amount} =req.body
+  getAccountScores(req, res) {
+    const { level, nickname } = req.body
+    const query = { nickname }
 
-      Levels.find({level},
-        (err,players)=>res.json(players))
-      .sort({score:-1}).lean().limit(players_amount).select("score nickname -_id")
-    }
+    if (level) query.level = level
+    //if level omitted, each level is found and returned
 
-    getRankFromScore(req,res){
-      const {level,score} =req.body
+    Levels.find(query, (err, c) => res.json(c))
+      .lean()
+      .select("level score -_id")
+  }
+  postLevelScore(req, res) {
+    const { score, nickname, level } = req.body
+    const query = { nickname, level }
+    const options = { upsert: true }
+    const update = { score: score }
+
+    //console.log(score,nickname,level)
+    Levels.updateOne(query, update, options, () => res.sendStatus(200))
+  }
+
+  getTopScores(req, res) {
+    const { level, players_amount } = req.body
+
+    Levels.find({ level }, (err, players) => res.json(players))
+      .sort({ score: -1 })
+      .lean()
+      .limit(players_amount)
+      .select("score nickname -_id")
+  }
+
+  getRankFromScore(req, res) {
+    const { level, score } = req.body
     //  console.time("time")
-      Levels.countDocuments({level,score:{$gt:score}},(err,rank)=>res.json(rank+1))
-   // await Levels.findOne({level,score:{$gte:score}},()=>{}).count()
-   //  console.timeEnd("time")
+    Levels.countDocuments({ level, score: { $gt: score } }, (err, rank) =>
+      res.json(rank + 1)
+    )
+    // await Levels.findOne({level,score:{$gte:score}},()=>{}).count()
+    //  console.timeEnd("time")
     // res.sendStatus(200)
-  //(err,count)=>console.log(count)
-    }
- 
+    //(err,count)=>console.log(count)
+  }
 }
 
 mongoose.connection.on("error", (error) => {
-   console.log("ERROR !", error)
+  console.log("ERROR !", error)
   process.exit(1)
 })
 
 mongoose.connection.on("connected", function () {
-   console.log("connected to mongo")
+  console.log("connected to mongo")
 })
 
 module.exports = DatabaseManager
-
-
 
 /*
     const p = []
@@ -189,4 +180,4 @@ Promise.all(p).then(()=>console.log("done"))
     
   })
 }
- */   
+ */
--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,7 @@
 // -------------- NOTE -------------
 // SERVER AND CLIENT ARE NOT SECURED AT ANY WAY WITH HELMET MODULE FOR EXAMPLE
 // THE GAME IS PRETTY SIMPLE AND I AM NOT AFRAID OF HACKERS IN THAT CASE
-
+require("dotenv").config()
 const compression = require("compression")
 const express = require("express")
 const cors = require("cors")
@@ -15,45 +15,39 @@ const DatabaseManager = require("./database-manager")
 
 const port = process.env.PORT || 3001
 const host = "0.0.0.0"
-const GAME_VERSION = 5;
+const GAME_VERSION = 5
 
-  const server = express()
-  server.use(compression())
-  server.use(cors())
-  server.use(bodyParser.json())
-  server.use(bodyParser.urlencoded({ extended: false }))
-  
+const server = express()
+server.use(compression())
+server.use(cors())
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: false }))
 
-  const databaseManager = new DatabaseManager()
-  
+const databaseManager = new DatabaseManager()
 
-  server.get("/getGameVersion",(req,res)=> res.json(GAME_VERSION));
-  server.get("/isServerAlive", (req, res) => res.sendStatus(200));
-  
-  server.get("/getConfigurations", (req, res) =>
+server.get("/getGameVersion", (req, res) => res.json(GAME_VERSION))
+server.get("/isServerAlive", (req, res) => res.sendStatus(200))
+
+server.get("/getConfigurations", (req, res) =>
   res.send({ skins_setup: customizeSkinsSetup, levels_config: levelsConfig })
 )
 
-  server.post("/createAccount",databaseManager.createAccount)
-  
-  server.post("/saveMoney",databaseManager.saveMoney)
-  
-  server.post("/saveNewSkin",databaseManager.saveNewSkin)
-  
-  server.post("/equipSkin",databaseManager.equipSkin)
-  
-  server.post("/getAccountProgress",databaseManager.getAccountProgress)
-  
-  server.post("/getAccountScores",databaseManager.getAccountScores)
-  
-  server.post("/getTopScores",databaseManager.getTopScores)
-  
-  server.post("/getRankFromScore",databaseManager.getRankFromScore)
-  
-  server.post("/postLevelScore",databaseManager.postLevelScore)
-  
-  server.listen(port, host, () => databaseManager.connectDatabase())
-  
+server.post("/createAccount", databaseManager.createAccount)
 
- 
+server.post("/saveMoney", databaseManager.saveMoney)
 
+server.post("/saveNewSkin", databaseManager.saveNewSkin)
+
+server.post("/equipSkin", databaseManager.equipSkin)
+
+server.post("/getAccountProgress", databaseManager.getAccountProgress)
+
+server.post("/getAccountScores", databaseManager.getAccountScores)
+
+server.post("/getTopScores", databaseManager.getTopScores)
+
+server.post("/getRankFromScore", databaseManager.getRankFromScore)
+
+server.post("/postLevelScore", databaseManager.postLevelScore)
+
+server.listen(port, host, () => databaseManager.connectDatabase())
